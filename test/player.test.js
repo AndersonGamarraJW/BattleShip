9ab0de0d814b7f2175fa342865e9b363d7d117ef
AndersonGamarraJW <andersong.v3105@gmail.com>
2024-03-20 20:37:50 +0100

    test(Player):Add add ship test

--- test/player.test.js
+++ test/player.test.js
@@ -1,4 +1,6 @@
 const Player = require("../src/Player.js");
+const Ship = require("../src/ship.js");
+
 describe("Player actions", () => {
   let player, player2;
   beforeEach(() => {
@@ -9,5 +11,16 @@ describe("Player actions", () => {
   test("Attack", () => {
     player.attack(1, 2, player2);
     expect(player2.board[1][2]).toBe(1);
+  });
+
+  test("Add Ship", () => {
+    const ship = new Ship({
+      point1: { x: 1, y: 1, status: true },
+      point2: { x: 1, y: 2, status: true },
+      point3: { x: 1, y: 3, status: true },
+    });
+    player.addShip(ship);
+    expect(player.qShips).toBe(1);
+    expect(player.getShip(0)).toEqual(ship);
   });
 });
