961f50906652483749fbc97e1c2aa23e31b59d52
AndersonGamarraJW <andersong.v3105@gmail.com>
2024-03-20 20:27:50 +0100

    feat(Player): Implement attack and recive attack methods

--- src/Player.js
+++ src/Player.js
@@ -1,7 +1,17 @@
 const GameBoard = require("./GameBoard.js");
-class Player {
+
+module.exports = class Player {
   constructor(name, sizeBoard) {
     this._name = name;
     this._board = new GameBoard(sizeBoard);
   }
-}
+  get board() {
+    return this._board.board;
+  }
+  attack(x, y, toPlayer) {
+    toPlayer.reciveAttack(x, y);
+  }
+  reciveAttack(x, y) {
+    this._board.reciveHit(x, y);
+  }
+};
