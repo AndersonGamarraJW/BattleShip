11c28aa5bd0ab5cc474913098af397321358a237
AndersonGamarraJW <andersong.v3105@gmail.com>
2024-03-21 02:35:36 +0100

    test(GameController):Add test for before init game

---
+++ test/gameController.test.js
@@ -0,0 +1,22 @@
+const Player = require("../src/Player.js");
+describe("GameController before game", () => {
+  let gameController;
+  beforeAll(() => {
+    gameController = new GameController();
+  });
+  test("Set size board", () => {
+    gameController.setSizeBoard(6);
+    expect(gameController.sizeBoard).toBe(6);
+  });
+  test("Add players", () => {
+    const player = new Player("player 1", 6);
+    const player2 = new Player("player 2", 6);
+    gameController.addPlayer(player);
+    expect(gameController.qPlayers).toBe(1);
+    gameController.addPlayer(2);
+    expect(gameController.qPlayers).toBe(2);
+  });
+});
+
+//In game section
+//After game section
