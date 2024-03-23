4d7a50e523872a0d063b124ca67d9872f71c6780
AndersonGamarraJW <andersong.v3105@gmail.com>
2024-03-23 02:09:13 +0100

    feat(GameBoard): Implment functions to pass test toglle turn of players

--- src/GameController.js
+++ src/GameController.js
@@ -1,7 +1,57 @@
-class GameController {
+module.exports = class GameController {
   constructor() {
     this._sizeBoard = 0;
     this._players = [];
+    /*
+     * Variable storing the current player to attack
+     * @type {Player}
+     * @private
+     */
+    this._currentPlayer = undefined;
+    /*
+     * Variable storing the target player to attack
+     * @type {Player}
+     * @private
+     */
+    this._targetPlayer = undefined;
+    /*
+     * Variable storing iterator values
+     * @type{int}
+     * @private
+     */
+    this._iteratorTurn = undefined;
+  }
+  _setupIteratorTurnVariable() {
+    this._iteratorTurn = this._getRandomNumber(0, this._players.length);
+  }
+  _incrementIteratorTurn() {
+    if (this._iteratorTurn >= this._players.length) this._iteratorTurn = 0;
+    else this._iteratorTurn += 1;
+  }
+  /*
+   * Function to set currentPlayer varible at start game
+   * @type{Function}
+   * @private
+   */
+  _setCurrentPlayer() {
+    this._currentPlayer = this._players[this._iteratorTurn];
+  }
+  /*
+   * Function to turn players
+   * @type{Function}
+   * @private
+   */
+  _toogleTurnPlayer() {
+    this._incrementIteratorTurn();
+    this._setCurrentPlayer();
+  }
+  /*
+   * Get random rumber between min and max limits
+   * @type{Function}
+   * @private
+   */
+  _getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   get sizeBoard() {
     return this._sizeBoard;
@@ -9,10 +59,24 @@ class GameController {
   get qPlayers() {
     return this._players.length;
   }
+  get currentPlayer() {
+    return this._currentPlayer;
+  }
+  get targetPlayer() {
+    return this._targetPlayer;
+  }
+  initGame() {
+    this._setupIteratorTurnVariable();
+    this._setCurrentPlayer();
+    // start timer
+  }
+  toogleTurn() {
+    this._toogleTurnPlayer();
+  }
   setSizeBoard(size) {
     this._sizeBoard = size;
   }
   addPlayer(player) {
     this._players.push(player);
   }
-}
+};
--- test/gameController.test.js
+++ test/gameController.test.js
@@ -1,3 +1,4 @@
+const GameController = require("../src/GameController.js");
 const Player = require("../src/Player.js");
 describe("GameController before game", () => {
   let gameController;
@@ -16,9 +17,30 @@ describe("GameController before game", () => {
     const player2 = new Player("player 2", 6);
     gameController.addPlayer(player);
     expect(gameController.qPlayers).toBe(1);
-    gameController.addPlayer(2);
+    gameController.addPlayer(player2);
     expect(gameController.qPlayers).toBe(2);
   });
 });
 //In game section
+describe("GameController in game", () => {
+  let gameController;
+  beforeAll(() => {
+    const player1 = new Player("player 1", 6);
+    const player2 = new Player("player 2", 6);
+    gameController = new GameController();
+    gameController.setSizeBoard(6);
+    gameController.addPlayer(player1);
+    gameController.addPlayer(player2);
+    gameController.initGame();
+  });
+  test("Toogle turn of players", () => {
+    const prevTurnPlayer = gameController.currentPlayer;
+    gameController.toogleTurn();
+    expect(prevTurnPlayer === gameController.currentTurnPlayer).toEqual(false);
+  });
+  test("Player attack other player", () => {
+    gameController.applyAttack(1, 2);
+    expect(gameController.targetPlayer.board[1][2]).toEqual(1);
+  });
+});
 //After game section
